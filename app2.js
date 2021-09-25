deacba77ac5fe063477fbdd5fdac739c7bd392dd
PaolaP <paolaperez8@gmail.com>
2021-09-25 11:53:31 -0400

    solution fixes

--- app2.js
+++ app2.js
@@ -1,11 +1,75 @@
+//date
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  //let year = date.getFullYear();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()]; //0 and 6
 
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentTime.getMonth()];
+  let dates = currentTime.getDate();
 
+  console.log(hours, minutes);
+  return `${hours}:${minutes} <br/> ${day} <br> ${month}, ${dates}`;
+}
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+
+dateElement.innerHTML = formatDate(currentTime);
+
+//to celcius
+function convertToCelcius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+//search form must include axios search
 
 function search(event) {
   event.preventDefault();
-  let apiKey = "c6410b310b8d22b3e3b3aa500da76687";
-  let city = "Paris";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemp);
+  let apiKey = "888e4fcf51eb407327068f47d5808891";
+  let city = `Paris`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  console.log(apiUrl);
+  //axios.get(apiUrl).then(showTemp);
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let fahrenheitLink = document.querySelector("#fahreheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", convertToCelcius);
+
